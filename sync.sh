55f09b9584f149ba449817f7d1a68a618ba1362e
Alan Feng <alanfeng_2020@qq.com>
2023-01-21 21:56:32 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -7,12 +7,12 @@ cp ./README.md ./public/README.md
 
 cd ./public
 
-wget -O ./GeoLite2-Country.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=${LICENSE_KEY}&suffix=tar.gz"
-wget -O ./GeoLite2-ASN.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=${LICENSE_KEY}&suffix=tar.gz"
-wget -O ./GeoLite2-City.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${LICENSE_KEY}&suffix=tar.gz"
+wget -q -O ./GeoLite2-Country.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=${LICENSE_KEY}&suffix=tar.gz"
+wget -q -O ./GeoLite2-ASN.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=${LICENSE_KEY}&suffix=tar.gz"
+wget -q -O ./GeoLite2-City.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${LICENSE_KEY}&suffix=tar.gz"
 tar zxvf ./GeoLite2-Country.tar.gz -C .
 tar zxvf ./GeoLite2-ASN.tar.gz -C .
-tar zxvf GeoLite2-City.tar.gz -C .
+tar zxvf ./GeoLite2-City.tar.gz -C .
 
 
 VERSION=$(ls | grep 'GeoLite2-Country_' | sed "s|GeoLite2-Country_||g" | tr -d '\n')
